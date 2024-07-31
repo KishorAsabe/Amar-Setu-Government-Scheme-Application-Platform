2e7dabdca1bf6cfa9ccb71a9be7c57f7366395cb
KishorAsabe <kishorasabe2020@gmail.com>
2024-07-31 19:14:46 +0530

    account type updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,7 @@
 export const ACCOUNT_TYPE = {
-  STUDENT: "Student",
-  INSTRUCTOR: "Instructor",
+  BENEFICIARY: "Beneficiary",
+  OPERATOR: "Operator",
   ADMIN: "Admin",
 }
 
-export const COURSE_STATUS = {
-  DRAFT: "Draft",
-  PUBLISHED: "Published",
-}
+
